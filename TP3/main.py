00cae95f9fbba4f36d96de496297e405ddfe484d
david <david.tabarie@gmail.com>
2019-04-04 10:37:55 +0200

    Basic functionnalities are working

--- TP3/main.py
+++ TP3/main.py
@@ -1,39 +1,57 @@
 #! /usr/bin/env python3
 
-import redis, requests, http, sys, flask
+import redis, http, sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
-# HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-
-  def do_GET(self):
-      self.send_response(200)
-
-      self.send_header('Content-type','text/html')
-      self.end_headers()
-
-      message = "Here are the notes:"
-      self.wfile.write(bytes(message, "utf8"))
-      return
-
-  def do_POST(self):
-      pass
-
-def run():
-  print('starting server...')
-
-  server_address = ("127.0.0.1", 8080)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-  print('running server...')
-  httpd.serve_forever()
-
-run()
+from flask import Flask, jsonify, request, redirect, url_for, make_response
 
 r = redis.Redis(
     host='localhost',
     port=6379,
     password='')
 
-r.set('4', 'note_victor')
-value = r.get('4')
-print(value)
+# Clear the database
+r.flushdb()
+
+# Debug insertions
+r.lpush('lst_notes', 'note_victor')
+r.lpush('lst_notes', 'note_david')
+r.lpush('lst_notes', 'note_pataya')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if request.method == 'GET':
+        return redirect(url_for('get_all'))
+
+@app.route('/notes/', methods=['GET'])
+def get_all():
+    content = []
+    alist = []
+    content.append(r.lrange('lst_notes', 0, -1))
+    content = str(content)
+    items_nb = r.llen('lst_notes')
+    for i in range(items_nb):
+        alist.append(i)
+    return jsonify(result = alist)
+
+@app.route('/notes/', methods=['POST'])
+def new_note():
+    r.rpush('lst_notes', request.data)
+    return 'ok'
+
+@app.route('/notes/<int:idnote>', methods=['GET'])
+def get_note(idnote):
+    return r.lindex('lst_notes', idnote)
+
+@app.route('/notes/idnote', methods=['DELETE'])
+def delete_note(idnote):
+    r.lrem('lst_notes', 0, idnote)
+    return 'ok'
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
